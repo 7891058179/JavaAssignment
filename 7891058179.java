582256f3cb9b762b2eeb2c973a5c20c7bdfac27f
7891058179 <37672415+7891058179@users.noreply.github.com>
2018-08-19 00:00:54 +0530

    adding 7891058179.java

---
+++ 7891058179.java
@@ -0,0 +1,14 @@
+class AndroidWorkDay1 {
+public static void main(String[] args)
+throws Exception{
+bufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str=br.readLine();
+int i, n=Integer.parserInt(br.readLine());
+String[] m= new String[n];
+for(i=0;i<n;i++)
+{ 
+m[i] = br.readLine();
+} 
+for(i=0;i<n;i++)
+ for(int j=-1; (j=str.indexOf(m[i], j+1)) !=-1;)
+System.out.println(j+" ");
